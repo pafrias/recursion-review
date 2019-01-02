523405186c0a75d4e199eba80c68c6fff606110e
pafrias <pafrias90@gmail.com>
2019-01-02 15:44:40 -0800

    All specs working for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return `'${obj}'`;
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return `${obj}`;
+  } else if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      var result = '[';
+      obj.map((arg)=>{
+        result += stringifyJSON(arg) + ',';
+      });
+      if (obj.length > 0) {
+        result = result.slice(0, result.length - 1);
+      }
+      result += ']';
+      return result;
+    } else {
+      var result = '{';
+      let keys = Object.keys(obj);
+      keys.map((arg)=>{
+        if (stringifyJSON(obj[arg])) {
+          result += stringifyJSON(arg) + ':' + stringifyJSON(obj[arg]) + ',';
+        }
+      });
+      if (result.length > 2) {
+        result = result.slice(0, result.length - 1);
+      }
+      result += '}';
+      return result;
+    }
+  }
 };
+  
